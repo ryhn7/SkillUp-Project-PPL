7cec4bfaf49dcbc94889773a0b39a4937f0e1375
Rif'an Fatoni Febrianto <rifaaanf@students.undip.ac.id>
2022-10-12 23:50:54 +0700

    update irs model

--- app/models/irs.model.js
+++ app/models/irs.model.js
@@ -25,27 +25,8 @@ const IRS = mongoose.model(
     mahasiswa: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'Mahasiswa',
-  },
-  smtAktif: {
-      type: Number,
-      required: [true, 'Semester aktif wajib diisi'],
-      maxlength: [2, 'Semester aktif maksimal 2 karakter'],
-  },
-  sks: {
-      type: Number,
-      required: [true, 'SKS wajib diisi'],
-      maxlength: [2, 'SKS maksimal 2 karakter'],
-  },
-  konfirmasi: {
-      type: String,
-      enum: ['Belum', 'Sudah'],
-      default: 'Belum',
-  },
-  file: {
-      type: String,
-      required: [true, 'Upload IRS wajib diisi'],
-  },
-}, {timestamps: true})
+    },
+})
 );
 
 module.exports = IRS;

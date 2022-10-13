6ad0575b33704f0e9ed4f209401a75f23c013164
Rif'an Fatoni Febrianto <73413289+Rifaaanf@users.noreply.github.com>
2022-10-13 11:49:11 +0700

    added irs, pkl, and skripsi controller

---
+++ app/controllers/khs.controller.js
@@ -0,0 +1,23 @@
+const db = require("../models");
+const Khs = db.khs;
+
+exports.submitKHS = (req, res) => {
+  const khs = new Khs({
+    semester_aktif: req.body.semester_aktif,
+    sks: req.body.sks,
+    sks_kumulatif: req.body.sks_kumulatif,
+    ip: req.body.ip,
+    ip_kumulatif: req.body.ip_kumulatif,
+    status_konfirmasi: req.body.status_konfirmasi,
+    file: req.file.path,
+    mahasiswa: req.mahasiswaId,
+  });
+
+  khs.save((err, khs) => {
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
+    res.send({ message: "KHS was uploaded successfully!" });
+  });
+};
--- app/models/index.js
+++ app/models/index.js
@@ -12,6 +12,9 @@ db.status = require("./status.model");
 db.provinsi = require("./provinsi.model");
 db.kabupaten = require("./kabupaten.model");
 db.irs = require("./irs.model");
+db.pkl = require("./pkl.model");
+db.khs = require("./khs.model");
+db.skripsi = require("./skripsi.model");
 
 db.ROLES = ["admin", "dosen", "mahasiswa", "departemen"];
 db.STATUS = [

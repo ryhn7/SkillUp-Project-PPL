ccd7e1c3ab4e91ad25a69e2af0479031795fafbc
Benhard Sim <36811298+BenhardSim@users.noreply.github.com>
2022-10-19 11:00:56 +0700

    fix parameter

--- app/controllers/skripsi.controller.js
+++ app/controllers/skripsi.controller.js
@@ -72,72 +72,68 @@ const fs = require("fs");
 // };
 
 exports.submitSkripsi = (req, res) => {
-    // buat instance skripsi
-    const skripsi = new Skripsi({
-        status: req.body.status,
-        nilai: req.body.nilai,
-        tanggal: req.body.tanggal,
-        semester: req.body.semester,
-        status_konfirmasi: req.body.status_konfirmasi,
-        file: req.file.path,
-        mahasiswa: req.mahasiswaId
-        status: req.body.status,
-    });
+  // buat instance skripsi
+  const skripsi = new Skripsi({
+    nilai: req.body.nilai,
+    tanggal: req.body.tanggal,
+    lama_studi: req.body.lama_studi,
+    status_konfirmasi: "belum",
+    file: req.file.path,
+    mahasiswa: req.mahasiswaId,
+  });
 
-    Skripsi.countDocuments(
-        {
+  Skripsi.countDocuments(
+    {
+      mahasiswa: skripsi.mahasiswa,
+    },
+    function (err, count) {
+      if (count === 0) {
+        skripsi.save((err, skripsi) => {
+          if (err) {
+            res.status(500).send({ message: err });
+            return;
+          }
+          res.send({ message: "Skripsi was uploaded successfully!" });
+        });
+      } else {
+        Skripsi.findOne(
+          {
             mahasiswa: skripsi.mahasiswa,
-        },
-        function (err, count) {
-            if (count === 0) {
-                skripsi.save((err, skripsi) => {
-                    if (err) {
-                        res.status(500).send({ message: err });
-                        return;
-                    }
-                    res.send({ message: "Skripsi was uploaded successfully!" });
-                });
-            } else {
-                Skripsi.findOne(
-                    {
-                        mahasiswa: skripsi.mahasiswa,
-                    },
-                    function (err, skripsi) {
-                        if (err) {
-                            res.status(500).send({ message: err });
-                            return;
-                        }
-                        fs.unlink(skripsi.file, function (err) {
-                            if (err) {
-                                res.status(500).send({ message: err });
-                                return;
-                            }
-                            Skripsi.updateOne(
-                                { _id: skripsi._id },
-                                {
-                                    $set: {
-                                        file: req.file.path,
-                                        nilai: req.body.nilai,
-                                        status: req.body.status,
-                                        status_konfirmasi: req.body.status_konfirmasi,
-                                        lama_studi: req.body.lama_studi,
-                                        tanggal: req.body.tanggal,
-                                    },
-                                },
-                                function (err, skripsi) {
-                                    if (err) {
-                                        res.status(500).send({ message: err });
-                                        return;
-                                    }
-                                    res.send({ message: "Skripsi was updated successfully!" });
-                                }
-                            );
-                        });
-                    }
-                );
+          },
+          function (err, skripsi) {
+            if (err) {
+              res.status(500).send({ message: err });
+              return;
             }
-        }
-    );
+            fs.unlink(skripsi.file, function (err) {
+              if (err) {
+                res.status(500).send({ message: err });
+                return;
+              }
+              Skripsi.updateOne(
+                { _id: skripsi._id },
+                {
+                  $set: {
+                    file: req.file.path,
+                    nilai: req.body.nilai,
+                    lama_studi: req.body.lama_studi,
+                    tanggal: req.body.tanggal,
+                  },
+                },
+                function (err, skripsi) {
+                  if (err) {
+                    res.status(500).send({ message: err });
+                    return;
+                  }
+                  res.send({ message: "Skripsi was updated successfully!" });
+                }
+              );
+            });
+          }
+        );
+      }
+    }
+  );
 };
 
 exports.getSkripsi = (req, res) => {
--- app/models/skripsi.model.js
+++ app/models/skripsi.model.js
@@ -1,35 +1,36 @@
 const mongoose = require("mongoose");
 
 const Skripsi = mongoose.model(
-  "Skripsi",
-  new mongoose.Schema({
-    nilai: {
-      type: String,
-      required: [true, "nilai wajib di isi"],
-      maxlength: [1, "max length 1"],
-    },
-    tanggal: {
-      type: Date,
-      required: [true, "tanggal harus di isi"],
-    },
-    semester: {
-      type: Number,
-      required: [true, "lama studi harus di isi"],
-    },
-    status_konfirmasi: {
-      type: String,
-      required: [true, "status konfirmasi harus di isi"],
-    },
-    file: {
-      // belum tipe data varbinary sementara string
-      type: String,
-      // required: [true, "upload skripsi harus di isi"],
-    },
-    mahasiswa: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Mahasiswa",
-    },
-  })
+
+    "Skripsi",
+    new mongoose.Schema({
+        nilai : { 
+            type : String,
+            required: [true, 'nilai wajib di isi'], 
+            maxlength : [1,"max length 1"]
+        },
+        tanggal : {
+            type : Date,
+            required: [true, "tanggal harus di isi"],
+        },
+        semester : {
+            type : Number,
+            required: [true, "lama studi harus di isi"],
+        },
+        status_konfirmasi : {
+            type : String,
+            required: [true, "status konfirmasi harus di isi"],
+        },
+        file : {
+            // belum tipe data varbinary sementara string 
+            type : String,
+            // required: [true, "upload skripsi harus di isi"],
+        },
+        mahasiswa: { 
+            type : mongoose.Schema.Types.ObjectId,
+            ref: "Mahasiswa",
+        }
+    })
 );
 
 module.exports = Skripsi;

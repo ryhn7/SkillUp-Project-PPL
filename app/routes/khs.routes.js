49d29d66da54a88e9c0928203664e490620c6633
gybran <charistomarcgybran@students.undip.ac.id>
2022-10-19 11:37:12 +0700

    perubahan sikit

--- app/routes/khs.routes.js
+++ app/routes/khs.routes.js
@@ -2,39 +2,39 @@ const { authJwt } = require("../middlewares");
 const controller = require("../controllers/khs.controller");
 
 module.exports = function (app) {
-  app.use(function (req, res, next) {
-    res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
+    app.use(function (req, res, next) {
+        res.header(
+            "Access-Control-Allow-Headers",
+            "x-access-token, Origin, Content-Type, Accept"
+        );
+        next();
+    });
+
+    app.post(
+        "/khs",
+        [authJwt.verifyToken, authJwt.isMahasiswa, authJwt.getMahasiswaId],
+        controller.submitKHS
     );
-    next();
-  });
 
-  app.post(
-    "/khs",
-    [authJwt.verifyToken, authJwt.isMahasiswa, authJwt.getMahasiswaId],
-    controller.submitKHS
-  );
+    app.get(
+        "/khs",
+        [authJwt.verifyToken, authJwt.isMahasiswa, authJwt.getMahasiswaId],
+        controller.getKHS
+    );
 
-  app.get(
-    "/khs",
-    [authJwt.verifyToken, authJwt.isMahasiswa, authJwt.getMahasiswaId],
-    controller.getKHS
-  );
+    app.get(
+        "/all-khs",
+        [authJwt.verifyToken, authJwt.isDepartemen],
+        controller.getAllKHS
+    );
 
-  app.get(
-    "/all-khs",
-    [authJwt.verifyToken, authJwt.isDepartemen],
-    controller.getAllKHS
-  );
-
-  app.get(
-    "/khs/:nim/:semester",
-    [
-      authJwt.verifyToken,
-      authJwt.isMahasiswaOrDosen,
-      authJwt.getMahasiswaIdFromNim,
-    ],
-    controller.downloadKHS
-  );
+    app.get(
+        "/khs/:nim/:semester",
+        [
+            authJwt.verifyToken,
+            authJwt.isMahasiswaOrDosen,
+            authJwt.getMahasiswaIdFromNim,
+        ],
+        controller.downloadKHS
+    );
 };

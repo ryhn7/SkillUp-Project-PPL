4ed2f9ed1032346ee4993d4a2502767b35a83fdf
gybran <charistomarcgybran@students.undip.ac.id>
2022-10-26 18:38:40 +0700

    ubh struktur get verify khs dosenwali + route download khs

--- app/controllers/khs.controller.js
+++ app/controllers/khs.controller.js
@@ -166,61 +166,28 @@ const waliKHS = async (req, res) => {
   const list_mhs = await Mahasiswa.find({ kodeWali: dosen._id });
   const list_khs = await Khs.find({});
 
-  let result = [];
-  for (let i = 0; i < list_mhs.length; i++) {
-    for (let j = 0; j < list_khs.length; j++) {
-      // cek tiap khs yang punya nilai mahasiswa == mahasiswa.id
-      if (list_mhs[i]._id.equals(list_khs[j].mahasiswa)) {
-        let obj_khs = {
-          id_khs: list_khs[j]._id,
-          nama: list_mhs[i].name,
-          semester_aktif: list_khs[j].semester_aktif,
-          sks: list_khs[j].sks,
-          sks_kumulatif: list_khs[j].sks_kumulatif,
-          ip: list_khs[j].ip,
-          ipk: list_khs[j].ip_kumulatif,
-          status_konfirmasi: list_khs[j].status_konfirmasi,
-          file: list_khs[j].flie,
-        };
-        result.push(obj_khs);
-      }
+    let result = [];
+    for (let i = 0; i < list_mhs.length; i++) {
+        for (let j = 0; j < list_khs.length; j++) {
+            // cek tiap khs yang punya nilai mahasiswa == mahasiswa.id
+            if (list_mhs[i]._id.equals(list_khs[j].mahasiswa)) {
+                let obj_khs = {
+                    id_khs: list_khs[j]._id,
+                    nama: list_mhs[i].name,
+                    semester_aktif: list_khs[j].semester_aktif,
+                    sks: list_khs[j].sks,
+                    sks_kumulatif: list_khs[j].sks_kumulatif,
+                    ip: list_khs[j].ip,
+                    ipk: list_khs[j].ip_kumulatif,
+                    status_konfirmasi: list_khs[j].status_konfirmasi,
+                    file: list_khs[j].flie,
+                };
+                result.push(obj_khs);
+            }
+        }
     }
-  }
-  res.status(200).send(result);
+    res.status(200).send(result);
 };
-// const waliKHS = async (req, res) => {
-//     const dosen = await Dosen.findOne({ user: req.userId });
-//     const list_mhs = await Mahasiswa.find({ kodeWali: dosen._id });
-//     const list_khs = await Khs.find({});
-
-//     let result = [];
-//     for (let i = 0; i < list_mhs.length; i++) {
-//         let khs_mahasiswa = [];
-
-//         for (let j = 0; j < list_khs.length; j++) {
-//             // cek tiap khs yang punya nilai mahasiswa == mahasiswa.id
-//             if (list_mhs[i]._id.equals(list_khs[j].mahasiswa)) {
-//                 let obj_khs = {
-//                     semester: list_khs[j].semester_aktif,
-//                     ip: list_khs[j].ip,
-//                     ipk: list_khs[j].ip_kumulatif,
-//                     status: list_khs[j].status_konfirmasi,
-//                 };
-
-//                 khs_mahasiswa.push(obj_khs);
-//             }
-//         }
-//         let obj_mahasiswa = {
-//             name: list_mhs[i].name,
-//             nim: list_mhs[i].nim,
-//             angkatan: list_mhs[i].angkatan,
-//             khs: khs_mahasiswa,
-//         };
-
-//         result.push(obj_mahasiswa);
-//     }
-//     res.status(200).send(result);
-// };
 
 const verifyKHS = async (req, res) => {
     const mhs = await Mahasiswa.findOne({ nim: req.params.nim });
